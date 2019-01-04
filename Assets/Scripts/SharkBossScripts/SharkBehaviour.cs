4c09e58880586b48b07fa2005c05ca6c14466dc6
LiamHernandezLucas <liam.hernandez.lucas@student.howest.be>
2019-01-04 16:17:50 +0100

    SharkStart
    Added important bits for the shark

---
+++ Assets/Scripts/SharkBossScripts/SharkBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SharkBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    UnityEvent[] _phases;
+    UnityEvent _currentPhase;
+    int _currentIndex;
+
+    private void Update()
+    {
+        _currentPhase.Invoke();
+    }
+
+    public void NextPhase()
+    {
+        if (_phases[_currentIndex + 1] != null)
+            _currentPhase = _phases[_currentIndex + 1];
+        else _currentPhase = _phases[0];
+
+    }
+
+
+
+
+}
