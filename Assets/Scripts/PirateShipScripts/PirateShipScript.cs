15cc6c392bfbdec5ac9eeb6658a32ab71b73cb06
Faust Messely <faust.messely@student.howest.be>
2019-01-06 19:40:47 +0100

    Start Pirate Ship Level

---
+++ Assets/Scripts/PirateShipScripts/PirateShipScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateShipScript : MonoBehaviour {
+
+    public bool Active = false;
+    float timer = 0;
+    float _attackTimer = 0;
+    bool _spawnProcess = false;
+    int _tentacleHp = 50;
+    public bool despawnBoat = false;
+    PirateShipSpawningBehaviour _boss;
+    Healthpoints _healthPoints;
+    Animation anim;
+    ShipHP ship;
+    int _aanvalRnd;
+    // Use this for initialization
+    void Start()
+    {
+        anim = this.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _boss = transform.parent.gameObject.GetComponent<PirateShipSpawningBehaviour>();
+        _healthPoints = transform.parent.gameObject.GetComponent<Healthpoints>();
+        if (Active == false && _spawnProcess == false && _boss.boatsActive < _boss.maxBoatsAllowed)
+        {
+            timer = 0;
+            timer = Random.Range(1, 8);
+            _aanvalRnd = Random.Range(3, 6);
+            _spawnProcess = true;
+        }
+        else if (Active == false && _spawnProcess == true && _boss.boatsActive < _boss.maxBoatsAllowed)
+        {
+            if (timer >= 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                Active = true;
+                anim.Play("TentacleEmerge");
+                _spawnProcess = false;
+                _boss.boatsActive += 1;
+
+            }
+        }
+
+
+        if (Active && _tentacleHp > 0)
+        {
+            _attackTimer += Time.deltaTime;
+            if (_attackTimer >= _aanvalRnd)//DOE EEN AANVAL ALS DE TENTACEL MERGED IS
+            {
+                //  Debug.Log("ikvalaan");
+                _attackTimer = 0;
+                anim.Play("ShootLeft");
+                anim.Play("ShootRight");
+                Active = false;
+                _boss.boatsActive -= 1;
+                ship = GameObject.FindGameObjectWithTag("Ship").GetComponent<ShipHP>();
+                ship.ShipHitPoints -= 150;
+            }
+        }
+        else if (Active && _tentacleHp <= 0)//ALS TENTACLE MERGED IS EN HP IS 0 DAN DESPAWN
+        {
+            Debug.Log("tentaledood");
+            _boss.boatsActive -= 1;
+            _tentacleHp = 50;
+            despawnBoat = true;
+        }
+
+
+        if (despawnBoat)
+        {
+            //Debug.Log("despawn");
+            _attackTimer = 0;
+            Active = false;
+            despawnBoat = false;
+            anim.Play("TentacleSubmerge");
+        }
+
+    }
+
+
+    //ALS COLLISION ER IS MET BULLET DAN VERLIEST TENTACLE HP INDIEN HIJ LEVEND IS
+    //tentacle verliest leven
+    //boss hp verliest leven
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Bullet" && Active)
+        {
+            Debug.Log("dood");
+            _tentacleHp -= 50;
+            _healthPoints.maxHealth -= 50;
+        }
+    }
+}
---
+++ Assets/Scripts/PirateShipScripts/PirateShipSpawningBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateShipSpawningBehaviour : MonoBehaviour
+{
+    public short PirateShipDirections = 2;
+    public GameObject[] directions;
+    bool _allowLeftShip;
+    bool _allowRightShip;
+    int _BoatsInactiveCondition = 4;
+    public int boatsActive = 0;
+    public int maxBoatsAllowed = 2;
+
+    CheckManager _checkSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ronde 1 spawn alle tentacles - als tentacle 10sec ni geraakt wordt val aan 50hp kanon doet 50 los object 25
+        //Laad bools in die aantonen of de tentacles of head kunnen komen
+        _checkSpawn = GameObject.FindGameObjectWithTag("CheckManager").GetComponent<CheckManager>();
+        _allowLeftShip = _checkSpawn.AllowLeftTentacle;
+        _allowRightShip = _checkSpawn.AllowRightTentacle;
+
+        if (_allowLeftShip == false)
+        {
+            if (directions[1].GetComponent<PirateShipScript>().Active)
+            {
+                directions[1].GetComponent<PirateShipScript>().despawnBoat = true;
+            }
+        }
+
+        if (_allowRightShip == false)
+        {
+            if (directions[0].GetComponent<PirateShipScript>().Active)
+            {
+                directions[0].GetComponent<PirateShipScript>().despawnBoat = true;
+            }            
+        }
+    }
+}
