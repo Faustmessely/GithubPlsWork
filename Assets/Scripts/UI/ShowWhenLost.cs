b7c9773d49ed0bf6d9d3b58b5227cd04804ef16f
Faust Messely <faust.messely@student.howest.be>
2019-01-08 23:24:07 +0100

    UI/main menu Progress

---
+++ Assets/Scripts/UI/ShowWhenLost.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowWhenLost : MonoBehaviour {
+
+    public GameObject[] UIElements;
+    public GameObject ship;
+    private float _shipHealth;
+
+    private void Start()
+    {
+        foreach (GameObject go in UIElements)
+        {
+            go.SetActive(false);
+        }
+    }
+    void Update () {
+        _shipHealth = ship.GetComponent<ShipHP>().ShipHitPoints;
+
+        if (_shipHealth <= 0)
+        {
+            foreach (GameObject go in UIElements)
+            {
+                go.SetActive(true);
+            }
+        }
+	}
+}
