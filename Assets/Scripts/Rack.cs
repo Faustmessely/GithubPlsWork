86ee7e7c5f3f6d7bf90e0207e5bddbc77632218b
Realslife <brian.wouters@hotmail.com>
2019-01-13 02:32:11 +0100

    Big menus update
    Almost all menu's 100% done

--- Assets/Scripts/Rack.cs
+++ Assets/Scripts/Rack.cs
@@ -10,70 +10,85 @@ public class Rack : MonoBehaviour {
     //[SerializeField] Collider Trigger;
     List<GameObject> _bulletsInChest;
     List<GameObject> _bulletsInCollider;
+    List<GameObject> _bulletsInUse;
+    int CountInUse = 0;
     GameObject _currentBullet;
     int _previousStack = 0;
+
+
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         _bulletsInChest = new List<GameObject>();
         _bulletsInCollider = new List<GameObject>();
+        _bulletsInUse = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       // Debug.Log("Bulletcollider count dus mogelijke bullets:" + _bulletsInCollider.Count);
         AddChestItem();
 
     }
 
     void AddChestItem()
     {
-        //while( _bulletsInChest.Count < _maxStack && _bulletsInCollider.Count > 0)//Als kist niet vol is en er zitten objecten in de kist trigger blijft objecten in kist steken
-        //{
-        //    if (!_bulletsInCollider[0].GetComponent<Bullet>().InGebruik)//verplaats object van lijst als het niet in gebruik is en er plaats is in de chest
-        //    {
-        //        for(int i = 0;i < _bulletsInCollider.Count)
-        //        _bulletsInChest.Add(_bulletsInCollider[0]);//Voeg toe aan chest lijst
-        //        BulletPlacement(_bulletsInCollider[0]);//Voeg eigenschappen toe voor de kist
-        //        _bulletsInCollider.RemoveAt(0);//Verwijder van normale lijst
-        //    }
-        //    else
-        //    {
-
-        //    }
-            
-        //}
+        while (_bulletsInChest.Count < _maxStack && _bulletsInCollider.Count > 0)//Als kist niet vol is en er zitten objecten in de kist trigger blijft objecten in kist steken
+        {       
+            if (!_bulletsInCollider[0].GetComponent<Bullet>().InGebruik)//Zet object in chest als deze niet in gebruik is
+            {
+                Debug.Log(_bulletsInCollider.Count);
+                Debug.Log("1KEER GEACTIVEERD MAAR FUNCTIE MIS SMEER?");
+                _bulletsInChest.Add(_bulletsInCollider[0]);//Voeg toe aan chest lijst
+                BulletPlacement(_bulletsInCollider[0]);//Voeg eigenschappen toe voor in de kist te zitten    
+                _bulletsInCollider.RemoveAt(0);//Verwijder van collider lijst
+            }
+            else//Als object wel in gebruik is zet het in de _bulletsInUse lijst
+            {
+                _bulletsInUse.Add(_bulletsInCollider[0]);//Zet in use lijst    
+                _bulletsInCollider.RemoveAt(0);//Verwijder van collider lijst
+                   
+            }
+        }
     }
 
     void BulletPlacement(GameObject currentBullet)
     {
-            // Debug.Log(_bulletsInChest.Count + " " + _previousStack);
             if (_bulletsInChest.Count > _previousStack)//enkel position aanpassen als chest count verhoogd maar niet als die verlaagd
             {
-
-            currentBullet.GetComponent<Rigidbody>().isKinematic = true;
-            currentBullet.transform.position = positions[_bulletsInChest.Count].transform.position;
-            _previousStack = _bulletsInChest.Count;
-             //   Debug.Log(_bulletsInChest.Count + " " + _previousStack);
+                    currentBullet.GetComponent<Rigidbody>().isKinematic = true;
+                    currentBullet.transform.position = positions[_bulletsInChest.Count].transform.position;
+                    _previousStack = _bulletsInChest.Count;
             }
-
     }
 
     private void OnTriggerEnter(Collider col)//on trigger enter werkt maar 1keer dus als hij erin gaat moet die direkt gedetecteert worden
-    {
-       
+    {      
         if (col.transform.name == "Bullet")
         {
-            _bulletsInCollider.Add(col.gameObject);             
+            _bulletsInCollider.Add(col.gameObject);
         }
-
-
-
     }
 
+    private void OnTriggerStay(Collider col)//Als bullet in de trigger blijft en niet meer in gebruik is
+    {
+        if (col.transform.name == "Bullet")
+        {        
+            //Als object in collider zit maar niet in gebruik is remove van ingebruik lijst en zet terug in collider lijst
+            if (!col.GetComponent<Bullet>().InGebruik && _bulletsInUse.Contains(col.gameObject))
+            {
+                Debug.Log("tedyffff");
+                //Verwijder van list met gebruike items en zet terug in list met normale colliders           
+                _bulletsInCollider.Add(col.gameObject);
+                _bulletsInUse.Remove(col.gameObject);
+            }
+        }
+    }
 
     private void OnTriggerExit(Collider col)
     {
-        //Voeg de distance vector toe van elk collision object waarbij de tag Interactable is en als er nog geen targetObject geselecteerd is
+        //Verwijder de bullet uit elke lijst als bullet uit de collider gaat
         if (col.transform.name == "Bullet")
         {
             if (_bulletsInCollider.Contains(col.gameObject))
@@ -86,7 +101,11 @@ public class Rack : MonoBehaviour {
                 _bulletsInChest.Remove(col.gameObject);
                 _previousStack = _bulletsInChest.Count;
             }
-            //  Debug.Log("AAAAAA");
+
+            if (_bulletsInUse.Contains(col.gameObject))
+            {
+                _bulletsInChest.Remove(col.gameObject);
+            }
         }
     }
 }
---
+++ Assets/Scripts/UI/LevelCreation.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LevelCreation : MonoBehaviour {
+
+    //LevelDisplay
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] Button[] arrows;
+    [SerializeField] Sprite[] spritesLevelName;
+    [SerializeField] string[] levelNames;
+    [SerializeField] Image levelTitle;
+    [SerializeField] GameObject _playerMenu;
+    public GameObject levelDisplay;
+    Button _levelDisplayButton;
+    private Image _levelDisplayImage;
+    int _levelDisplayImgNumberIndex;
+    float _horizontalDirection;
+    public EventSystem eventSystem;
+    StandaloneInputModule standAloneInputModule;
+    public string level1, level2, level3;
+    
+    //Delay
+    float _delay;
+    float _inputDelayTimer;
+    bool _isInputOnDelay;
+
+    void Start ()
+    {
+        standAloneInputModule = eventSystem.GetComponent<StandaloneInputModule>();
+       _levelDisplayButton = levelDisplay.GetComponent<Button>();
+       _levelDisplayImage = levelDisplay.GetComponent<Image>();
+        _levelDisplayImgNumberIndex = 0;
+        levelTitle.sprite = spritesLevelName[_levelDisplayImgNumberIndex];
+        _levelDisplayImage.sprite = sprites[_levelDisplayImgNumberIndex];
+        _delay = standAloneInputModule.repeatDelay;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+        //Bewegen van sprites Links en Rechts
+        _horizontalDirection = Input.GetAxis("Horizontal_P1");
+      
+        //als input on delay wordt geactiveert
+        if(_isInputOnDelay && _horizontalDirection > 0.4f || _horizontalDirection < -0.4f)
+        {
+            _inputDelayTimer += Time.deltaTime;
+            if(_inputDelayTimer >= _delay)
+            {
+                _isInputOnDelay = false;
+                _inputDelayTimer = 0f;
+            }
+        }
+        else if(_isInputOnDelay && _horizontalDirection < 0.5f || _horizontalDirection > -0.5f)
+        {
+            _inputDelayTimer = 0;
+            _isInputOnDelay = false;
+        }
+
+
+        if (_levelDisplayButton.gameObject == eventSystem.currentSelectedGameObject && !_isInputOnDelay)// && _timeSinceLastInput >= _delay
+        {
+            if (_horizontalDirection > 0.4f)
+            {
+                _levelDisplayImgNumberIndex++;
+               // arrows[0]. = arrows[0].OnPointerDown;
+                _isInputOnDelay = true;
+            }
+            else if (_horizontalDirection < -0.4f)
+            {
+                _levelDisplayImgNumberIndex--;
+
+                _isInputOnDelay = true;
+            }
+        }
+
+        //Looping index
+        if (_levelDisplayImgNumberIndex > sprites.Length-1)
+        {
+            _levelDisplayImgNumberIndex = 0;
+        }
+        else if (_levelDisplayImgNumberIndex < 0)
+        {
+            _levelDisplayImgNumberIndex = sprites.Length-1;
+        }
+
+        //Afbeelding weergeven
+        _levelDisplayImage.sprite = sprites[_levelDisplayImgNumberIndex];
+        levelTitle.sprite = spritesLevelName[_levelDisplayImgNumberIndex];
+
+    }
+
+
+    public void ChangeScene() // make sure to attach this to event trigger
+    {
+        switch (_levelDisplayImgNumberIndex)
+        {
+
+            case 0:
+                SceneManager.LoadScene(levelNames[_levelDisplayImgNumberIndex]);
+                break;
+            case 1:
+                SceneManager.LoadScene(levelNames[_levelDisplayImgNumberIndex]);
+                break;
+            case 2:
+                SceneManager.LoadScene(levelNames[_levelDisplayImgNumberIndex]);
+                break;
+            default:
+                Debug.Log("Error");
+                break;  
+        }
+    }
+}
